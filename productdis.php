337d4e7cb3e86baebcd8ea0a7e1257d0d467af4a
mohan122 <35259724+mohan122@users.noreply.github.com>
2018-04-22 19:59:30 +0530

    Add files via upload

---
+++ productdis.php
@@ -0,0 +1,71 @@
+
+
+<?php
+    header('Access-Control-Allow-Origin:*');
+   // Define database connection parameters
+   $hn      = 'localhost';
+   $un      = 'root';
+   $pwd     = '';
+   $db      = 'digimart';
+   $cs      = 'utf8';
+
+   // Set up the PDO parameters
+   $dsn 	= "mysql:host=" . $hn . ";port=3306;dbname=" . $db . ";charset=" . $cs;
+   $opt 	= array(
+                        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                        PDO::ATTR_EMULATE_PREPARES   => false,
+                       );
+ 
+   $pdo 	= new PDO($dsn, $un, $pwd, $opt);
+   $data    = array();
+   $datak=array();
+  
+
+
+ $json    =  file_get_contents("php://input");
+ $obj   =  json_decode($json);
+ $key     =  strip_tags($obj->key);
+switch($key)
+   {
+      case "create":
+
+ $EMAIL		     =    filter_var($obj->piece, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW);
+
+
+   try {
+     
+    
+          $stmt1= $pdo->query("INSERT INTO productpurchase(SELECT EMAIL,emailf FROM itemregister WHERE emailf IN (SELECT NAME FROM register WHERE LATITUDE BETWEEN (select minlat FROM locate WHERE sno IN (SELECT MAX(sno) FROM locate)) AND (select maxlat FROM locate WHERE sno IN (SELECT MAX(sno) FROM locate)) AND LONGITUDE BETWEEN (select minlon FROM locate WHERE sno IN (SELECT MAX(sno) FROM locate)) AND (select maxlon FROM locate WHERE sno IN (SELECT MAX(sno) FROM locate)) ))"); 
+
+   $stmt 	= $pdo->query("SELECT itemname FROM productpurchase WHERE itemname= '$EMAIL'");
+
+ if ($row  = $stmt->fetch(PDO::FETCH_OBJ))
+   {
+         
+        $stmt2=$pdo->query("INSERT INTO productbuying(item) SELECT EMAIL FROM itemregister WHERE EMAIL= '$EMAIL' AND emailf IN (SELECT mailid FROM productpurchase) "); 
+     
+          $stmt2=$pdo->query("INSERT INTO productbuying(shopname) SELECT SHOPNAME FROM register WHERE NAME IN (select mailid FROM productpurchase) "); 
+ 
+
+        
+     
+   }
+
+    else
+   {
+      
+        echo json_encode($data);
+
+    }
+
+
+     
+   }
+   catch(PDOException $e)
+   {
+      echo $e->getMessage();
+   }
+}
+
+?>
